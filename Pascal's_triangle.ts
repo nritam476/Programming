ed3fe23040f7645b0b977e07f8b143cbb09e745b
nritam476 <45099327+nritam476@users.noreply.github.com>
2020-08-12 23:31:54 +0530

    commit1

---
+++ Pascal's_triangle.ts
@@ -0,0 +1,29 @@
+function getRow(rowIndex: number): number[] {
+    let x=[1,1];
+    for(let i=0;i<rowIndex-1;i++){
+        
+        
+        let z=[]
+        let y=0;
+        z.push(1);
+        for(let h=0;h<x.length-1;h++){
+       
+        z.push(x[h]+x[h+1]);
+        
+        
+        }
+        z.push(1);
+        x=z;
+    }
+
+if(rowIndex==0){
+return [1];
+}
+else if(rowIndex==1){
+return [1,1];
+}
+else{
+return x;
+}
+};
+
